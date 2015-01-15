354924555970657c3ebeb397fd8be95ddc9961be
Musa Abu Al-Hawa <musa16@meet.mit.edu>
2015-01-15 20:06:07 +0200

    oop.py

---
+++ oop.py
@@ -0,0 +1,13 @@
+class Animal:
+	def __init__(self,name,age,color,size):
+		self.name =name
+		self.age =age
+		self.color =color
+		self.size =size
+
+dog=Animal("sadek",22,"Yellow","tiny")
+print(dog.name)
+print(dog.age)
+print(dog.color)
+print(dog.size)
+
